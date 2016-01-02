bc68c4967e9b780c99986bf4dc19accb05b7f6b2
Olga Bobrova <oleyka@gmail.com>
2016-01-02 11:54:30 +0000

    static is static

--- prepaw/prepaw/heroku-settings.py
+++ prepaw/prepaw/heroku-settings.py
@@ -18,9 +18,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
