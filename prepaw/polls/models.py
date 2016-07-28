e8e9432794cb4c2720880e03f13e93697ef47d1d
Olga Bobrova <oleyka@gmail.com>
2016-07-28 01:40:21 -0700

    week 5: tests

--- prepaw/polls/models.py
+++ prepaw/polls/models.py
@@ -1,10 +1,17 @@
 from __future__ import unicode_literals
+
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __unicode__(self):
         return self.question_text
--- prepaw/polls/views.py
+++ prepaw/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 import pytz
 
 from .models import Choice, Question
@@ -13,13 +14,24 @@ class IndexView(generic.ListView):  # pylint: disable=too-many-ancestors
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):  # pylint: disable=too-many-ancestors
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):  # pylint: disable=too-many-ancestors
