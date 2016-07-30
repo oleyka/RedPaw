d41d8824c2fd4641199225ed59dd5d1dbc46072e
Olga Bobrova <oleyka@gmail.com>
2016-07-30 12:46:08 -0700

    w6: add some static

--- prepaw/prepaw/settings.py
+++ prepaw/prepaw/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'g2ed)$l6l3^u7+$nv^!gde6#oburmz84de4i=%5%hv5wn5mjdk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# ALLOWED_HOSTS = ['*']
-# DEBUG = False
+ALLOWED_HOSTS = ['*']
+DEBUG = False
 
-DEBUG = True
-ALLOWED_HOSTS = []
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -106,6 +106,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
