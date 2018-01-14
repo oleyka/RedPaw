311c89a8c35a588ef8ffcd013723ab9301f139a2
Olga Bobrova <oley@dragonia.info>
2018-01-14 00:24:32 -0800

    reset the polls app; update some freebsd stuff

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 venv:
 	virtualenv -p python3 venv
-	. venv/bin/activate && pip install -Ir requirements.txt
+	. venv/bin/activate && venv/bin/pip install -Ir requirements.txt
 
 freeze: venv requirements.in
-	. venv/bin/activate && pip install pip-tools
-	. venv/bin/activate && pip-compile requirements.in
+	. venv/bin/activate && venv/bin/pip install pip-tools
+	. venv/bin/activate && venv/bin/pip-compile requirements.in
+
+run: venv
+	python prepaw/manage.py runserver 192.168.42.42:8000
 
 clean:
 	rm -rf .tox
--- prepaw/polls/views.py
+++ prepaw/polls/views.py
@@ -1,66 +1,12 @@
-# from django.template import loader
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, render, get_object_or_404
-from django.core.urlresolvers import reverse
-from django.views import generic
-from django.utils import timezone
-import pytz
+from django.shortcuts import render
 
-from .models import Choice, Question
+# Create your views here.
+
+from django.http import HttpResponse
 
 
-class IndexView(generic.ListView):  # pylint: disable=too-many-ancestors
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
-    def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):  # pylint: disable=too-many-ancestors
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):  # pylint: disable=too-many-ancestors
-    model = Question
-    template_name = 'polls/results.html'
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def set_timezone(request):
-    if request.method == 'POST':
-        request.session['django_timezone'] = request.POST['timezone']
-        return redirect('/')
-    else:
-        return render(request, 'polls/template.html', {'timezones': pytz.common_timezones})
+def blah(request):
+    return HttpResponse("Hello, blah-blah.")
